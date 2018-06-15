673e59cd7d689004920da5498dd68290b309fc16
Tyler Standley <tyler.standley21@gmail.com>
2018-06-15 10:16:46 -0700

    iterating date span to capture weekday + date

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const moment = require("moment");
 const util = require("./utilities.js");
 
 const app = express();
@@ -8,8 +9,25 @@ const port = 4000;
 app.use(bodyParser.json());
 
 // capture the date + number of days data
-app.get("/:MM/:DD/:YYYY/:days", function(req, res) {
-  console.log(req.params);
+app.get("/:month/:day/:year/:span", function(req, res) {
+  // format input for moment
+  let startDate = moment(
+    req.params.month + "/" + req.params.day + "/" + req.params.year,
+    "MM/DD/YYYY"
+  );
+
+  for (let i = 0; i < req.params.span; i++) {
+    console.log(
+      moment(startDate)
+        .add(i, "days")
+        .format("MM/DD/YYYY")
+    );
+    console.log(
+      moment(startDate)
+        .add(i, "days")
+        .format("dddd")
+    );
+  }
   res.end();
 });
 
